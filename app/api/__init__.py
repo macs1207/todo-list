d9fe43ddab6f0ffd3babecc540c68532455147b5
macs1207 <kenginlim@gmail.com>
2021-12-01 22:56:26 +0800

    feat(api): remove user api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,8 @@
 from flask import Blueprint
 from .auth import auth_bp
-from .user import user_bp
 from .task import task_bp
 
 api_bp = Blueprint('api', __name__)
 
 api_bp.register_blueprint(auth_bp)
-api_bp.register_blueprint(user_bp)
 api_bp.register_blueprint(task_bp)
