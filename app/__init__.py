1f7662a3df40d59c7b79dae1cbb926a9c5b2bf36
macs1207 <kenginlim@gmail.com>
2021-12-03 10:18:27 +0800

    feat(web): add front-end file and route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -10,7 +10,8 @@ migrate = Migrate()
 jwt = JWTManager()
 
 def create_app(config_name='development'):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='dist',
+                template_folder='dist', static_url_path='/')
     CORS(app)
     app.config.from_object(config[config_name])
 
@@ -19,5 +20,9 @@ def create_app(config_name='development'):
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-
+    
+    @app.route('/', methods=['GET'])
+    def index():
+        return render_template('index.html')
+        
     return app
