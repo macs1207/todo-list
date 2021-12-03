a09a289a44a00c85d3a8c72cb6021bd64955c7b4
macs1207 <kenginlim@gmail.com>
2021-12-03 10:06:08 +0800

    feat(api): add task CRUD

--- app/api/task.py
+++ app/api/task.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from datetime import datetime
+from ..model import User, Task
 
 task_bp = Blueprint('task', __name__)
 
@@ -7,33 +9,90 @@ task_bp = Blueprint('task', __name__)
 @task_bp.route('/tasks', methods=['GET'])
 @jwt_required()
 def get_tasks():
-    # TODO:
-    return 'ok'
+    identity = get_jwt_identity()
+    query_tasks = User.find_tasks_by_username(identity['username'])
+    tasks_json = [task.get_task() for task in query_tasks]
+    return {
+        'status': 'success',
+        'data': tasks_json
+    }
 
 
 @task_bp.route('/tasks', methods=['POST'])
 @jwt_required()
 def create_task():
-    # TODO:
-    return 'ok'
+    try:
+        title = request.json['title']
+        content = request.json['content']
+        done = request.json['done']
+        expire_at = request.json['expire_at']
+    except KeyError:
+        return {
+            'status': 'fail',
+            'detail': 'Missing params.'
+        }, 400
+    except TypeError:
+        return {
+            'status': 'fail',
+            'detail': 'Missing params.'
+        }, 400
+
+    identity = get_jwt_identity()
+    user_id = identity['uid']
+    expire_at_parse = datetime.strptime(expire_at, "%Y-%m-%d %H:%M")
+    Task.create_task(title, content, done, expire_at_parse, user_id)
+    return {
+        'status': 'success'
+    }
 
 
-@task_bp.route('/task/<int:id>', methods=['GET'])
+# @task_bp.route('/task/<int:id>', methods=['GET'])
+# @jwt_required()
+# def get_task(id):
+#     # TODO:
+#     return 'ok'
+
+
+@task_bp.route('/task/<int:tid>', methods=['PATCH'])
 @jwt_required()
-def get_task(id):
-    # TODO:
-    return 'ok'
+def update_task(tid):
+    try:
+        title = request.json['title']
+        content = request.json['content']
+        done = request.json['done']
+        expire_at = request.json['expire_at']
+    except KeyError:
+        return {
+            'status': 'fail',
+            'detail': 'Missing params.'
+        }, 400
+    except TypeError:
+        return {
+            'status': 'fail',
+            'detail': 'Missing params.'
+        }, 400
+
+    identity = get_jwt_identity()
+    user_id = identity['uid']
+    expire_at_parse = datetime.strptime(expire_at, "%Y-%m-%d %H:%M")
+    if Task.update_task(tid, title, content, done, expire_at_parse, user_id):
+        return {
+            'status': 'success'
+        }
+    return {
+        'status': 'fail'
+    }, 403
 
 
-@task_bp.route('/task/<int:id>', methods=['PATCH'])
+@task_bp.route('/task/<int:tid>', methods=['DELETE'])
 @jwt_required()
-def update_task(id):
-    # TODO:
-    return 'ok'
-
-
-@task_bp.route('/task/<int:id>', methods=['DELETE'])
-@jwt_required()
-def delete_task(id):
-    # TODO:
-    return 'ok'
+def delete_task(tid):
+    identity = get_jwt_identity()
+    user_id = identity['uid']
+    if Task.delete_task(tid, user_id):
+        return {
+            'status': 'success'
+        }
+    return {
+        'status': 'fail'
+    }, 403
--- app/model/__init__.py
+++ app/model/__init__.py
@@ -7,20 +7,20 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'user'
-    
+
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(24), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     create_at = db.Column(db.DateTime, default=datetime.now)
     update_at = db.Column(
         db.DateTime, default=datetime.now, onupdate=datetime.now)
-    
+
     user_task = db.relationship("Task", backref="user")
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-    
+
     @classmethod
     def create_user(cls, username, password):
         query = cls.query.filter_by(username=username).first()
@@ -32,11 +32,15 @@ class User(db.Model):
         db.session.add(user)
         db.session.commit()
         return True
-    
+
     @classmethod
     def find_user_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
-    
+
+    @classmethod
+    def find_tasks_by_username(cls, username):
+        return cls.find_user_by_username(username).user_task
+
     @staticmethod
     def validate_password(password, hashed_password):
         if bcrypt.hashpw(password, hashed_password) == hashed_password:
@@ -46,7 +50,7 @@ class User(db.Model):
 
 class Task(db.Model):
     __tablename__ = 'task'
-    
+
     tid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.String(255), nullable=False)
@@ -55,10 +59,51 @@ class Task(db.Model):
     create_at = db.Column(db.DateTime, default=datetime.now)
     update_at = db.Column(
         db.DateTime, default=datetime.now, onupdate=datetime.now)
-    
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.uid'), nullable=False)
 
-    def __init__(self, title, content, expire_at):
+    def __init__(self, title, content, done, expire_at, user_id):
         self.title = title
         self.content = content
+        self.done = done
         self.expire_at = expire_at
+        self.user_id = user_id
+
+    def get_task(self):
+        return {
+            'tid': self.tid,
+            'title': self.title,
+            'content': self.content,
+            'done': self.done,
+            'expire_at': self.expire_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'create_at': self.create_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'update_at': self.update_at.strftime('%Y-%m-%d %H:%M:%S')
+        }
+
+    @classmethod
+    def create_task(cls, title, content, done, expire_at, user_id):
+        task = cls(title, content, done, expire_at, user_id)
+        db.session.add(task)
+        db.session.commit()
+        return True
+
+    @classmethod
+    def update_task(cls, tid, title, content, done, expire_at, user_id):
+        query = cls.query.filter_by(tid=tid).first()
+        if user_id == query.user_id:
+            query.title = title
+            query.content = content
+            query.done = done
+            query.expire_at = expire_at
+            db.session.commit()
+            return True
+        return False
+    
+    @classmethod
+    def delete_task(cls, tid, user_id):
+        query = cls.query.filter_by(tid=tid).first()
+        if user_id == query.user_id:
+            db.session.delete(query)
+            db.session.commit()
+            return True
+        return False
